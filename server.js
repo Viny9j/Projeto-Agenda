285306e1d14fa2cfe2c4cd77e0f06c94bbfc9267
Viny <vinyalves1307@gmail.com>
2023-10-23 19:42:57 -0300

    helmet removido

--- server.js
+++ server.js
@@ -18,12 +18,9 @@ const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-// const helmet = require('helmet');
 const csrf = require('csurf')
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-// app.use(helmet());
-// app.use(helmet.referrerPolicy({ policy: ["origin", "unsafe-url"] }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
